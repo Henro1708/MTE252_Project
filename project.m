85421f8cf9a6d99408f43838370dcf6aafc6414a
thatlittlemoonfromstarwars <116924824+thatlittlemoonfromstarwars@users.noreply.github.com>
2024-10-10 19:01:33 -0400

    Some improvements. Still need to work on it to meet project requirements

--- project.m
+++ project.m
@@ -1,3 +1,6 @@
+clc
+clear
+
 function [mono,n_out] = stereo_to_mono(stereo)
     % Translate file from stereo to mono
     % Input: Stereo -> audio matrix in a nx2
@@ -11,37 +14,37 @@ function [mono,n_out] = stereo_to_mono(stereo)
     end
     mono = stereo;
 end
-function play_audio(audio, Fs)
-    % Plays audio
-    % Inputs: 
-    % audio -> audio to play
-    % Fs -> Factor scale 
-
-    %plot(audio);
-    soundsc(audio,Fs);
-end
-
-function [cos, t] = get_cos(n)
-    % Plays audio
-    % Input = n -> len of function
-    % Outputs = Cos -> cossine function with same lenght as sample
-    t = linspace(1,n,n);
-    cos = t;
-    return
-end
 
 % input file
 filename = 'Input_mp3/Words regular voice street.mp3';
+
+% read audio from file
 [y,Fs] = audioread(filename);
-expected_Fs = 16000;
+% combine stereo (2D array) to mono (1D array)
 [audio,n] = stereo_to_mono(y);
-[cos,t] = get_cos(n);
-plot(cos,t);
-audio = resample(audio,expected_Fs,Fs);
 
-play_audio(audio,0.33*Fs)
+% resample audio
+desired_Fs = 16000;
+audio = resample(audio,desired_Fs,Fs);
+
+% plot and play audio
+figure(1)
+soundsc(audio,desired_Fs);
+plot(audio)
+hold on
+
+% calculate cosine function
+t = linspace(1,n/3,desired_Fs);
+y_cos = cos(2*pi/1000*t);
+
+% plot and play cosine function
+figure(2)
+soundsc(y_cos, desired_Fs)
+plot(t, y_cos);
+xlim([0 2000])
+hold on
 
 
-
-
-
+% TODO:
+% - consolidate into one function that can be run on multiple files
+% - add capability to write sound to a new file
