1a9557258c73f875ba125cca79000fb64c58160a
thatlittlemoonfromstarwars <116924824+thatlittlemoonfromstarwars@users.noreply.github.com>
2024-10-28 19:23:38 -0400

    Completed Phase 1 Project Requirements

--- project.m
+++ project.m
@@ -1,69 +1,127 @@
 clc
 clear
 
-function [mono,n_out] = stereo_to_mono(stereo)
+% adds input data folder path to MATLAB
+addpath("Input_mp3\")
+
+function [mono, n_out] = stereo_to_mono(stereo)
     % Translate file from stereo to mono
     % Input: Stereo -> audio matrix in a nx2
     % Output: Mono -> audio array in a single array nx1
-
     n = size(stereo);
     n_out = n(1);
-    if n(2) == 2  
-        mono = stereo(:,1) + stereo(:,2);
+    if n(2) == 2
+        mono = 0.5.*(stereo(:,1) + stereo(:,2));
         return
     end
     mono = stereo;
 end
-function play_audio(audio, Fs)
-    % Plays audio
-    % Inputs: 
-    % audio -> audio to play
-    % Fs -> Factor scale 
 
-    %plot(audio);
-    soundsc(audio,Fs);
-end
-
-function plot_audio(audio)
-    % plots audio
-    % Inputs: 
-    % audio -> audio to be plotted
+function save_audio(full_name, audio, Fs)
+    % Save audio data to file
+    % Input:
+    % full_name - full name of original audio file (includes parent folder)
+    % audio - actual audio data
+    % Fs - sampling frequency
     
+    % define save folder
+    save_folder = 'Resampled_files/';
+    
+    % split original save folder name and file name 
+    full_name_split = split(full_name, '/');
+
+    % isolated file name
+    file_name = full_name_split(length(full_name_split));
+
+    % append folder name with file name
+    final_name = strcat(save_folder, file_name);
+
+    % write to file
+    audiowrite(final_name, audio, Fs);
+end
+
+function play_and_plot_cos(n, Fs, figure_num)
+    % Plays a 1 kHz cosine wave and then plots it as a function of time
+    % over 2 cycles
+    % Input:
+    % n - number of samples
+    % Fs - sampling frequency
+    cos_freq = 1000; % in Hz
+    
+    % Cosine as a function of sample number
+    s = 0:1:n;
+    cos_s = cos(2 * pi * cos_freq / Fs * s); % cosine as a function of sample number
+    
+    % Cosine as a function of sample time
+    t = (0:n-1) / Fs;
+    cos_t = cos(2 * pi * cos_freq * t); % cosine as a function of time
+    
+    % Plot cosine function as a function of time
+    figure(abs(figure_num+1));
+    plot(t,cos_t);
+    xlabel('Time (s)');
+    xlim([0, 0.002])
+    ylabel('Amplitude');
+    title('Plot of 1kHz Cosine Wave');
+    hold on;
+    soundsc(cos_s, Fs);
+end
+
+function process_audio(filename, figure_num)
+    % Read in audio file
+    [y, Fs] = audioread(filename);
+    
+    % Print out original sampling rate
+    fprintf('Original Sampling Rate: %u \n', Fs);
+
+    % Convert from stereo to mono
+    [audio, n] = stereo_to_mono(y);
+    
+    % Resample audio to 16000 kHz
+    desired_Fs = 16000;
+    audio = resample(audio, desired_Fs, Fs);
+    n = length(audio);
+    
+    % Save audio to file
+    save_audio(filename, audio, desired_Fs);
+    
+    % Play audio file
+    soundsc(audio, desired_Fs);
+
+    % Plot audio waveform as a function of sample number
+    figure(abs(figure_num));
     plot(audio);
-    xlabel('Sample Rate')
-    ylabel('Audio Waveform')
-    title('Plot of Recording')
+    xlabel('Sample Number')
+    ylabel('Amplitude')
+    title('Plot of Recording Resampled to 16kHz')
+    hold on;
+
+    % Plot and play cosine function
+    play_and_plot_cos(n, desired_Fs, figure_num)
+    
 end
 
-function save_audio(name, audio, Fs)
-    name_start = 'Saved_files/';
-    name_end = '.mp3'
-    name_total = strcat(name_start, name, name_end);
-    audiowrite(name_total,audio,Fs)
-end
+% % Process all files
+% % array of input file names
+% filenames = ["Input_mp3/Conversation low voice street.mp3", ...
+%              "Input_mp3/Conversation regular voice street.mp3", ...
+%              "Input_mp3/Convo high voice quiet.mp3", ...
+%              "Input_mp3/Convo low voice quiet.mp3", ...
+%              "Input_mp3/Convo regular voice quiet.mp3", ...
+%              "Input_mp3/Convo reg voice conversation.mp3", ...
+%              "Input_mp3/Words high voice quiet.mp3", ...
+%              "Input_mp3/Words high voice street.mp3", ...
+%              "Input_mp3/Words low voice street.mp3", ...
+%              "Input_mp3/Words low voice quiet.mp3", ...
+%              "Input_mp3/Words regular voice street.mp3", ...
+%              "Input_mp3/Words reg voice conversation.mp3", ...
+%              "Input_mp3/Words reg voice quiet.mp3"];
+% num_files = length(filenames);
+% figure_num = 1;
+% for i = 1:num_files
+%     process_audio(filenames(i), figure_num);
+%     figure_num = figure_num + 2;
+% end
 
-function play_plot_cos(n, Fs)  % Needs to be finished to be 1Khz
-    t = linspace(0,n,1000); ''
-    cos_out = cos(t);
-    play_audio(cos_out,Fs);
-end
-
-function main_audio(filename)
-    [y,Fs] = audioread(filename);
-    expected_Fs = 16000;
-    [audio,n] = stereo_to_mono(y);
-    play_plot_cos(n, expected_Fs);
-    audio = resample(audio,expected_Fs,Fs);
-    plot_audio(audio);
-
-    %play_audio(audio,expected_Fs)
-    save_audio(filename, audio, expected_Fs);
-end
-
-% input file
-filenames = ["Input_mp3/Words regular voice street.mp3","Input_mp3/Conversation low voice street.mp3","Input_mp3/Conversation Regular voice street.mp3","Input_mp3/Convo high voice quiet.mp3","Input_mp3/Convo low voice quiet.mp3","Input_mp3/Convo regular voice quiet.mp3","Input_mp3/Convo reg voice conversation.mp3","Input_mp3/Words high voice quiet.mp3","Input_mp3/Words high voice street.mp3","Input_mp3/Words low voice street.mp3","Input_mp3/Words low voice quiet.mp3","Input_mp3/Words regular voice street.mp3","Input_mp3/Words reg voice conversation.mp3","Input_mp3/Words reg voice quiet.mp3"]
-n_of_files = length(filenames);
-for i = 1:n_of_files
-    filenames(i)
-    main_audio(filenames(i))
-end
+% Process one file
+process_audio("Input_mp3/Conversation low voice street.mp3", 1);
